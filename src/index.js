578b7f3ab700f2475be97310de2503e7c5abd007
MatheusSouzaTech <matheusestudotech@gmail.com>
2025-12-10 19:22:02 -0300

    Configuração da função inicial para consumo da api e processamento dos dados

--- src/index.js
+++ src/index.js
@@ -8,12 +8,13 @@ const path = require('path')
 const URL_API = "https://jsonplaceholder.typicode.com/posts"
 const FILE = 'dados.txt'
 
-function processData(posts){
+//Configurando a função de processamento de dados
+function processData(posts) {
     const totalPost = posts.length
     let tituloBase = ''
 
-    for (const posts of post){
-        if(posts.title.length > tituloBase.length){
+    for (const post of posts) {
+        if (post.title.length > tituloBase.length) {
             tituloBase = post.title
         }
     }
@@ -24,4 +25,40 @@ function processData(posts){
 
     return dados
 }
+
+async function main() {
+    try {
+        //configurando o consumo da API
+        const response = await axios.get(URL_API)
+        const posts = response.data
+
+        //exibição no console
+        console.log("Dados recebidos")
+        console.log(posts[0])
+        console.log(`\nRequisição realizada com sucesso! Total de itens: ${posts.length}`) 
+
+        //Processamento dos Dados
+        const summaryContent = processData(posts)
+
+        //Salvamento em Arquivo
+        const outputPath = path.join(__dirname, '..', 'data', FILE)
+
+        //Garantir que a pasta 'data' exista
+        await fs.mkdir(path.dirname(outputPath), { recursive: true })
+
+        // Salva o resumo
+        await fs.writeFile(outputPath, summaryContent, 'utf8')
+
+        //Mensagem de sucesso
+        console.log(`\n✅ Sucesso: O resumo foi salvo em ${outputPath}`)
+    }
+    catch (error) {
+        console.error('\nErro ao executar a CLI:', error.message)
+        if (error.response) {
+            console.error('Status HTTP:', error.response.status)
+        }
+    }
+}
+
+main()
 
