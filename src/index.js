0d5cb1f76b2eaabb9b4c26beac96f18513305eb0
MatheusSouzaTech <matheusestudotech@gmail.com>
2025-12-10 18:42:55 -0300

    importação das bibliotecas e colocando api no codigo e definindo o nome do arquivo

---
+++ src/index.js
@@ -0,0 +1,12 @@
+//Importando as biliotecas e recursos necessarios
+const axios = require('axios')
+const fs = require('fs/promises')
+const path = require('path')
+
+
+//importando api para o codigo e criando o nome do arquivo principal
+const URL_API = "https://jsonplaceholder.typicode.com/posts"
+const FILE = 'dados.txt'
+
+
+
